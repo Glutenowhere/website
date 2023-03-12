ae72e5837274662b7f474f014f93e464eb0a2945
SFRL <sebastian.lobbers@googlemail.com>
2023-03-12 19:11:12 +0000

    setup gatsby-node and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,35 +1,78 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/layout";
+import { GatsbyImage } from "gatsby-plugin-image";
+
 
 const About = () => {
+    const data = useStaticQuery(graphql`
+      {
+        bio: markdownRemark(
+          fields: { category: { eq: "about" } }
+          fileAbsolutePath: { regex: "/bio.md/" }
+        ) {
+          html
+          frontmatter {
+            title
+            image {
+              childImageSharp {
+                gatsbyImageData(layout: CONSTRAINED, width: 400)
+              }
+            }
+          }
+        }
+        project: markdownRemark(
+          fields: { category: { eq: "about" } }
+          fileAbsolutePath: { regex: "/project.md/" }
+        ) {
+          html
+          frontmatter {
+            title
+            image {
+              childImageSharp {
+                gatsbyImageData(layout: CONSTRAINED, width: 400)
+              }
+            }
+          }
+        }
+      }
+    `);
   return (
     <Layout>
-      <section>
+      <section className="section">
         <div className="columns">
-          <div className="column is-one-third"></div>
+          <div className="column is-one-third">
+            <GatsbyImage
+              alt="Image of Anne Steinhoff"
+              image={data.bio.frontmatter.image.childImageSharp.gatsbyImageData}
+            />
+          </div>
           <div className="column is-two-thirds">
-            When I got diagnosed with Coeliac Disease at the beginning of my
-            University studies, I felt relieved knowing that my suffering had a
-            name, and that there were more people like me out in the world. The
-            feeling of relief changed once I learnt that my symptoms had changed
-            little, that a restrictive diet as a young woman at University was a
-            dealbreaker in friendships and relationships, and that the disease
-            was misunderstood in society. I decided to focus on my studies,
-            earning a Bachelor and Master degree. I did not know how to work
-            full-time with a body that showed persistent and life- affecting
-            symptoms, and colleagues who quizzed me about it. For several years,
-            I worked in Finance related roles at two Russell Group Universities-
-            always close to giving in. What kept me going? The belief that I
-            wasn’t trying hard enough! Finding the chronic illness community
-            online made me realise that the problem was not me but that our
-            workplaces do not treat employees as human beings. I thought about
-            all the experiences I make daily with Coeliac Disease. I started
-            wondering how other employees with Coeliac Disease feel. Over the
-            time, I got more confident about demanding that our workplaces
-            should listen to our experiences. What if it is time to build a
-            platform to hear these stories? I enrolled for a PhD at the
-            University of Essex to research these experiences: What is YOUR
-            worklife living with Coeliac Disease like?
+            <h1 className="title">{data.bio.frontmatter.title}</h1>
+            <div
+              className="content"
+              dangerouslySetInnerHTML={{ __html: data.bio.html }}
+            ></div>
+          </div>
+        </div>
+      </section>
+      <hr />
+      <section className="section">
+        <div className="columns">
+          <div className="column is-two-thirds">
+            <h1 className="title">{data.project.frontmatter.title}</h1>
+            <div
+              className="content"
+              dangerouslySetInnerHTML={{ __html: data.project.html }}
+            ></div>
+          </div>
+          <div className="column is-one-third">
+            <GatsbyImage
+              alt="Image describing the Coeliac Disease and the Workplace project"
+              image={
+                data.project.frontmatter.image.childImageSharp.gatsbyImageData
+              }
+            />
           </div>
         </div>
       </section>
